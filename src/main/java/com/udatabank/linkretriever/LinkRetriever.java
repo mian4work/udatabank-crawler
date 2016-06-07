1979fcc02c37e7352cebd26c4efcd3c89723d542
mian4work <mian4work@yahoo.com>
2016-06-07 14:45:33 -0700

    convert from json to obj

--- src/main/java/com/udatabank/linkretriever/LinkRetriever.java
+++ src/main/java/com/udatabank/linkretriever/LinkRetriever.java
@@ -1,9 +1,23 @@
 package com.udatabank.linkretriever;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.List;
+
 /**
  * Created by miazhang on 6/7/16.
  */
 public class LinkRetriever {
 
+    public static List<CompanyAnnouncement> getAnnouncements() throws Exception {
+
+        String json = JsonRetriever.getJson();
+
+        Gson gson = new Gson();
+        final List<CompanyAnnouncement> announcements = gson.fromJson(json, new TypeToken<List<CompanyAnnouncement>>(){}.getType());
+
+        return announcements;
+    }
 }
