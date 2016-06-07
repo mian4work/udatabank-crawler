289b313980e9cbff8cb38922dbbd097450f8d056
mian4work <mian4work@yahoo.com>
2016-06-07 15:03:56 -0700

    some testing code seems to work

--- src/main/java/com/udatabank/linkretriever/LinkRetriever.java
+++ src/main/java/com/udatabank/linkretriever/LinkRetriever.java
@@ -2,21 +2,19 @@ package com.udatabank.linkretriever;
 
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.util.List;
 
 /**
  * Created by miazhang on 6/7/16.
  */
 public class LinkRetriever {
 
-    public static List<CompanyAnnouncement> getAnnouncements() throws Exception {
+    public static String[][] getAnnouncements() throws Exception {
 
         String json = JsonRetriever.getJson();
 
         Gson gson = new Gson();
-        final List<CompanyAnnouncement> announcements = gson.fromJson(json, new TypeToken<List<CompanyAnnouncement>>(){}.getType());
+//        final List<CompanyAnnouncement> announcements = gson.fromJson(json, new TypeToken<List<CompanyAnnouncement>>(){}.getType());
+        final String[][] announcements = gson.fromJson(json, String[][].class);
 
         return announcements;
     }
@@ -25,13 +23,13 @@ public class LinkRetriever {
 
         try {
 
-            List<CompanyAnnouncement> announcements = LinkRetriever.getAnnouncements();
+            String[][] announcements = LinkRetriever.getAnnouncements();
 
-            for(CompanyAnnouncement announcement : announcements) {
-
-                System.out.println(announcement.getTitle());
-                System.out.println(announcement.getUrl());
-            }
+//            for(CompanyAnnouncement announcement : announcements) {
+//
+//                System.out.println(announcement.getTitle());
+//                System.out.println(announcement.getUrl());
+//            }
         }
         catch (Exception ex) {
             ex.printStackTrace();
